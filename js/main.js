14eec62f61b68d19fd5c3b9cfb37bcbbd539cb3e
Keven Teles <glevenkeven@gmail.com>
2022-12-15 19:04:37 -0300

    feat: size measurements have been updated and style added in boxes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const buttonOpenMenu = document.querySelector('.buttonOpenMenu')
+import { navHeroJs } from "./navHero.js";
 
-const menu = document.querySelector('.menu-list')
-
-const menuBtn = document.querySelector('#open-menu-btn');
-const closeBtn = document.querySelector('#close-menu-btn');
-
-menuBtn.addEventListener('click', () => {
-  menu.style.display = 'flex';
-  closeBtn.style.display = 'inline-block';
-  menuBtn.style.display = 'none';
-});
-
-//close menu nav
-const closeNav = () => {
-  menu.style.display = 'none';
-  closeBtn.style.display = 'none';
-  menuBtn.style.display = 'inline-block';
-};
-closeBtn.addEventListener('click', closeNav);
+navHeroJs()
